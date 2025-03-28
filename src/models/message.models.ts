1ab39a3b6ce3b3f1ec4dbbd02e3e9bedcd3a5abd
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-03-28 12:56:24 +0500

    add message model

---
+++ src/models/message.models.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IMessage extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+export const MessageSchema: Schema<IMessage> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export const MessageModel =
+  (mongoose.models.Message as mongoose.Model<IMessage>) ||
+  mongoose.model<IMessage>("Message", MessageSchema);
