9114b2642513a4953b634f1240c805494fd28c9c
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-04-01 22:41:53 +0500

    add route validation in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,37 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
 
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXT_AUTH_SEC,
+  });
+  const { pathname } = request.nextUrl;
+
+  // If user is authenticated and visits public pages, redirect to dashboard
   if (
     token &&
-    ( url.pathname.startsWith("/signin")  ||
-      url.pathname.startsWith("/signup") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname.startsWith("/"))
+    (pathname === "/" ||
+      pathname.startsWith("/signin") ||
+      pathname.startsWith("/signup") ||
+      pathname.startsWith("/verify"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  
+  // If user is not authenticated and visits /verify or /dashboard, redirect to signin
+  if (
+    !token &&
+    (pathname.startsWith("/dashboard"))
+  ) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  // Otherwise, allow the request to proceed
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    "/signin",
-    "/signup",
-    "/",
-    "/verify",
-    "/dashboard/:path*",
-    "/verify/:path*",
-  ],
+  matcher: ["/", "/signin", "/signup", "/verify/:path*", "/dashboard/:path*"],
 };
