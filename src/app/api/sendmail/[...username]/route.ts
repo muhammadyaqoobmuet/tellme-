ab2e22270ba22f28891e3147bcf91b1a9fd2d716
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-04-01 22:44:01 +0500

    add resend mail functionality

---
+++ src/app/api/sendmail/[...username]/route.ts
@@ -0,0 +1,50 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { UserModel } from "@/models/user.model";
+import nodemailer from "nodemailer";
+
+export async function GET(
+  request: Request,
+  context: { params: { username: string } }
+) {
+  // Convert the incoming request to JSON
+  const { params } = context;
+  const username = params?.username; //
+
+  await dbConnect();
+  const user = await UserModel.findOne({ username });
+  const verificationCode = user?.verifyCode;
+  // Create the transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "yaqoobahmed45700@gmail.com",
+      pass: process.env.APP_PASSWORD,
+    },
+  });
+
+  // Setup email options
+  const mailOptions = {
+    from: "yaqoobahmed45700@gmail.com",
+    to: user?.email,
+    subject: "Verify Your Email Address",
+    text: `Your verification code is: ${verificationCode}`,
+  };
+
+  try {
+    // Send the email
+    await transporter.sendMail(mailOptions);
+    return new Response(
+      JSON.stringify({ message: "Email sent successfully!" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        message: "Failed to send email",
+        error: "at sending mail ",
+      }),
+      { status: 500 }
+    );
+  }
+}
